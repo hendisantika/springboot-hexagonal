8f6498bd93644d65390d34f7f02e38fe87c4ffaf
Hendi Santika <hendisantika@gmail.com>
2019-02-02 13:49:17 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboothexagonal/SpringbootHexagonalApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springboothexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHexagonalApplication.class, args);
+    }
+
+}
+
